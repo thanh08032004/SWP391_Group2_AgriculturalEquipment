9fb1e2ab7ea4c3823adaeef0e03f47c771c1ed3c
cuongnguyen <cuongducjerry@gmail.com>
2026-01-10 18:31:49 +0700

    "Add dal/DBContext"

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Acer
+ */
+public class DBContext {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/agri_cms"
+          + "?useSSL=false"
+          + "&allowPublicKeyRetrieval=true"
+          + "&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
