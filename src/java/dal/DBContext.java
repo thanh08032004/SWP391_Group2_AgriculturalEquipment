b24fd156f85dd47c0fc1a0bfa4cb7f5347881a54
cuongnguyen <cuongducjerry@gmail.com>
2026-01-19 10:50:28 +0700

    Fix ‎nbproject/project.properties

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -24,7 +24,7 @@ public class DBContext {
             + "&serverTimezone=Asia/Ho_Chi_Minh";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "123456";
 
     static {
         try {
